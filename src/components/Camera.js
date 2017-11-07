cd8daa0dd7e939cfae47dea49fb86152eb9afdc6
Jacob Orlando <Jake@Jacobs-MacBook-Pro-2.local>
2017-11-07 17:33:08 -0500

    I'm Committing

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Image, TouchableHighlight,Platform, StyleSheet, Text, View} from 'react-native';
+import {CameraRoll, Image, TouchableHighlight,Platform, StyleSheet, Text, View} from 'react-native';
 import Camera from 'react-native-camera';
+import ViewPhotos from './ViewPhotos';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,17 +14,33 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   capture: {
-
+        borderWidth: 5,
+        borderColor: 'black',
+        backgroundColor: 'white',
         position: 'absolute',
-        bottom: 0,
-        padding: 16,
-        right: 10,
-        left: 10,
-        borderRadius: 20,
+        bottom:-50,
+        right: 150,
+        left: 150,
+        height: 150,
+        width : 100,
+        borderRadius: 90 ,
+        marginLeft: -15,
+        marginRight: 0,
         alignItems: 'center',
-}});
+},
+cameraRoll: {
+        backgroundColor: 'white',
+        position: 'absolute',
+        bottom:10,
+        height: 50,
+        width : 50,
+        left: 300,
+        right: 100,
+        borderRadius: 25 ,
+}
+});
 
-export default class ChatScreen extends React.Component {
+export default class capture extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
@@ -41,22 +58,40 @@ export default class ChatScreen extends React.Component {
       .catch(err => console.error(err));
   }
 
+  getPhotosFromGallery() {
+     CameraRoll.getPhotos({ first: 1000000 })
+       .then(res => {
+         let photoArray = res.edges;
+         this.setState({ showPhotoGallery: true, photoArray: photoArray })
+       })
+   }
+
   render() {
+    if (this.state.showPhotoGallery) {
+      return (
+        <ViewPhotos
+          photoArray={this.state.photoArray} />
+      )
+    }
     return (
       <View style={styles.container}>
         <Camera
           ref={(cam) => { this.camera = cam;}}
           style={styles.preview}
-           aspect={Camera.constants.Aspect.fill}
-        >
-
-
+           aspect={Camera.constants.Aspect.fill}>
         </Camera>
+
         <TouchableHighlight
                     style={styles.capture}
                     onPress={this.takePicture.bind(this)}>
                     <Image source={require('../Img/Astley.gif')} />
-                </TouchableHighlight>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+                    style={styles.cameraRoll}
+                    onPress={() => this.getPhotosFromGallery()}>
+                    <Image source={require('../Img/camera_roll_circle.png')} />
+        </TouchableHighlight>
       </View>
     );
   }
