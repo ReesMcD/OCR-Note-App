6f532a33b8ee6bc6f297dd2a25f0e065bf350e00
ReesMcD <mcdevittrees@gmail.com>
2017-11-08 15:45:48 -0500

    Adding ios plist info

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button} from 'react-native';
-import RNCameraRoll from 'react-native-cameraroll';
-//import Camera from 'react-native-camera';
+////import Camera from 'react-native-camera';
 
 
-RNCameraRoll.getAssets({ assetType: 'image', limit: 20 })
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+// RNCameraRoll.getAssets({ assetType: 'image', limit: 20 })
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
 
 export default class EditScreen extends React.Component {
   constructor(props) {
